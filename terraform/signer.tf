22a4d37bb62849b8bbdb72624b37f336c1d56bb7
leonardodia3 <leonardo.diaz1130@gmail.com>
2026-06-23 15:31:47 -0500

    fix(terraform): agregar signing profiles para lambdas

---
+++ terraform/signer.tf
@@ -0,0 +1,24 @@
+resource "aws_signer_signing_profile" "register_student" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "register_student_profile"
+}
+
+resource "aws_signer_signing_profile" "list_students" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "list_students_profile"
+}
+
+resource "aws_signer_signing_profile" "attendance" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "attendance_profile"
+}
+
+resource "aws_signer_signing_profile" "manual_attendance" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "manual_attendance_profile"
+}
+
+resource "aws_signer_signing_profile" "attendance_history" {
+  platform_id = "AWSLambda-SHA384-ECDSA"
+  name        = "attendance_history_profile"
+}
