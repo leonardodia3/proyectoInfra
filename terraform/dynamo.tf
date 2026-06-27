22721ef0d11e36b2fbbad7a747f2b608cdff75af
leonardodia3 <leonardo.diaz1130@gmail.com>
2026-06-26 22:16:46 -0500

    Enhance DynamoDB configuration with encryption and recovery
    Added server-side encryption and point-in-time recovery settings for the DynamoDB table.

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -9,17 +9,24 @@ resource "aws_dynamodb_table" "attendance" {
   range_key = "sk"
 
   attribute {
-
-      name = "pk"
-      type = "S"
-
+    name = "pk"
+    type = "S"
   }
 
   attribute {
+    name = "sk"
+    type = "S"
+  }
 
-      name = "sk"
-      type = "S"
+  # CKV_AWS_119 - Cifrado con KMS CMK
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = aws_kms_key.dynamo_key.arn
+  }
 
+  # CKV_AWS_28 - Point-in-Time Recovery
+  point_in_time_recovery {
+    enabled = true
   }
 
 }
